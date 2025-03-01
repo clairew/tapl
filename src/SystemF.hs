1600857066af7185b6590e56c7812458c6f1a85d
Claire Wang <clairew@users.noreply.github.com>
2025-03-01 15:08:46 -0500

    SubstTypeInTerm

--- src/SystemF.hs
+++ src/SystemF.hs
@@ -54,6 +54,17 @@ freeTypeVars (TForall a t) = Set.delete a (freeTypeVars t)
 freeTypeVars TUnit = Set.empty
 freeTypeVars TNat = Set.empty
 
+freeTermVars :: Term -> Set.Set String
+freeTermVars (Var x) = Set.singleton x
+freeTermVars (Lam x _ e) = Set.delete x (freeTermVars e)
+freeTermVars (App e1 e2) = Set.union (freeTermVars e1) (freeTermVars e2)
+freeTermVars (TyAbs _ e) = freeTermVars e
+freeTermVars (TyApp e _) = freeTermVars e
+freeTermVars Unit = Set.empty
+freeTermVars Zero = Set.empty
+freeTermVars (Succ e) = freeTermVars e
+freeTermVars (IsZero e) = freeTermVars e
+
 substType :: String -> Type -> Type -> Type  
 substType a s (TVar b) 
     | a == b = s
@@ -66,3 +77,21 @@ substType a s (TForall v t)
         in TForall fresh (substType a s (substType v (TVar fresh) t))
 substType a s (TUnit) = TUnit
 substType a s (TNat) = TNat
+
+substTypeInTerm :: String -> Type -> Term -> Term 
+substTypeInTerm a s (Var x) = Var x
+substTypeInTerm a s (Lam x t e) = Lam x (substType a s t) (substTypeInTerm a s e)
+substTypeInTerm a s (TyAbs x t) 
+    | a == x = TyAbs x t 
+    | otherwise = 
+        if Set.notMember x $ freeTypeVars s 
+            then TyAbs x (substTypeInTerm a s t)
+            else
+                let fresh = freshVar x (Set.union(freeTypeVars s) (Set.fromList [a]))
+                in TyAbs fresh (substTypeInTerm a s (substTypeInTerm x (TVar fresh) t))
+substTypeInTerm a s (App e1 e2) = App (substTypeInTerm a s e1) (substTypeInTerm a s e2) 
+substTypeInTerm a s (TyApp t typ) = TyApp (substTypeInTerm a s t) (substType a s typ)
+substTypeInTerm _ _ Unit = Unit
+substTypeInTerm _ _ Zero = Zero
+substTypeInTerm a s (Succ e) = Succ (substTypeInTerm a s e)
+substTypeInTerm a s (IsZero e) = IsZero (substTypeInTerm a s e)
