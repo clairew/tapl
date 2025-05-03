348b38229619e4a3e31dd96baa2a754b0b8c2003
Claire Wang <clairew@users.noreply.github.com>
2025-05-03 19:03:11 -0400

    fix bounds in f omega sub

--- src/FOmegaSub.hs
+++ src/FOmegaSub.hs
@@ -8,7 +8,7 @@ import Control.Monad.State
 data Type
     = TVar String
     | TArrow Type Type
-    | TForall String Type
+    | TForall String Type Type
     | TAbs String Kind Type
     | TApp Type Type
     | TTop
@@ -18,7 +18,7 @@ data Term
     = Var String
     | Lam String Type Term
     | App Term Term
-    | TyAbs String Kind Term
+    | TyAbs String Type Term
     | TyApp Term Type 
     deriving (Show, Eq)
 
@@ -29,7 +29,7 @@ data Kind
 
 data Context = Context
     { termVars :: [(String, Type)]
-    , typeVars :: Map.Map String (Kind, Type)
+    , typeVars :: Map.Map String (Kind, Type) -- (Kind, Bound)
     } deriving (Show, Eq)
 
 emptyContext :: Context
@@ -38,8 +38,8 @@ emptyContext = Context [] Map.empty
 extendContext :: String -> Type -> Context -> Context 
 extendContext var typ (Context terms types) = Context ((var, typ):terms) types 
 
-extendTypeVar :: String -> Kind -> Context -> Context 
-extendTypeVar var k (Context terms types) = Context terms (Map.insert var (k, bound) types)
+extendTypeVarBound :: String -> Type -> Kind -> Context -> Context 
+extendTypeVarBound var bound kind (Context terms types) = Context terms (Map.insert var (kind, bound) types)
 
 lookupVar :: String -> Context -> Maybe Type
 lookupVar x (Context terms _) = lookup x terms
@@ -48,7 +48,7 @@ lookupTypeVarKind :: String -> Context -> Maybe Kind
 lookupTypeVarKind x (Context _ types) = fst <$> Map.lookup x types
 
 lookupTypeVarBound :: String -> Context -> Maybe Type 
-lookupTypeVarKind x (Context _ types) = snd <$> Map.lookup x types
+lookupTypeVarBound x (Context _ types) = snd <$> Map.lookup x types
 
 typeVarInContext :: String -> Context -> Bool
 typeVarInContext a (Context _ types) = Map.member a types
@@ -59,7 +59,7 @@ freshVar x vars = head $ filter (\v -> Set.notMember v vars) $ map (\n -> x ++ r
 freeTypeVars :: Type -> Set.Set String
 freeTypeVars (TVar a) = Set.singleton a
 freeTypeVars (TArrow t1 t2) = Set.union (freeTypeVars t1) (freeTypeVars t2)
-freeTypeVars (TForall a t) = Set.delete a (freeTypeVars t)
+freeTypeVars (TForall a _ t) = Set.delete a (freeTypeVars t)
 freeTypeVars (TApp t1 t2) = Set.union (freeTypeVars t1) (freeTypeVars t2)
 freeTypeVars (TAbs a _ t) = Set.delete a (freeTypeVars t)
 
@@ -75,11 +75,11 @@ substType a s (TVar b)
     | a == b = s
     | otherwise = TVar b
 substType a s (TArrow t1 t2) = TArrow (substType a s t1) (substType a s t2)
-substType a s (TForall v t)
-    | a == v = TForall v t
-    | otherwise = if Set.notMember v (freeTypeVars s) then TForall v (substType a s t)
+substType a s (TForall v b t)
+    | a == v = TForall v b t
+    | otherwise = if Set.notMember v (freeTypeVars s) then TForall v (substType a s b) (substType a s t)
         else let fresh = freshVar v (Set.union (freeTypeVars t) (freeTypeVars s))
-        in TForall fresh (substType a s (substType v (TVar fresh) t))
+        in TForall fresh (substType a s b) (substType a s (substType v (TVar fresh) t))
 substType a s (TAbs v k t)
     | a == v = TAbs v k t
     | otherwise = if Set.notMember v (freeTypeVars s) then TAbs v k (substType a s t)
@@ -90,20 +90,20 @@ substType a s (TApp t1 t2) = TApp (substType a s t1) (substType a s t2)
 substTypeInTerm :: String -> Type -> Term -> Term
 substTypeInTerm a s (Var x) = Var x
 substTypeInTerm a s (Lam x t e) = Lam x (substType a s t) (substTypeInTerm a s e)
-substTypeInTerm a s (TyAbs x k t)
- | a == x = TyAbs x k t
+substTypeInTerm a s (TyAbs x bound t)
+ | a == x = TyAbs x (substType a s bound) t  -- If a is the bound variable, only substitute in the bound
  | otherwise =
      let bindsX = case s of
-             TForall y _ | x == y -> True
+             TForall y _ _ | x == y -> True
              _ -> False
          needsRename = bindsX || Set.member x (freeTypeVars s)
      in if needsRename
          then
              let fresh = freshVar x (Set.union (freeTypeVars s) (Set.singleton a))
                  t' = substTypeInTerm x (TVar fresh) t
-             in TyAbs fresh k (substTypeInTerm a s t')
+             in TyAbs fresh (substType a s bound) (substTypeInTerm a s t')
          else
-             TyAbs x k (substTypeInTerm a s t)
+             TyAbs x (substType a s bound) (substTypeInTerm a s t)
 substTypeInTerm a s (App e1 e2) = App (substTypeInTerm a s e1) (substTypeInTerm a s e2)
 substTypeInTerm a s (TyApp t typ) = TyApp (substTypeInTerm a s t) (substType a s typ)
 
@@ -174,7 +174,8 @@ typeEquiv tyS tyT =
     in case (tyS', tyT') of 
         (TVar i, TVar j) -> i == j
         (TArrow s1 s2, TArrow t1 t2) -> typeEquiv s1 t1 && typeEquiv s2 t2 
-        (TForall x1 s2, TForall x2 t2) -> 
+        (TForall x1 b1 s2, TForall x2 b2 t2) ->
+            typeEquiv b1 b2 && 
             let t2' = substType x2 (TVar x1) t2 
             in typeEquiv s2 t2'
         (TAbs x1 k1 t1, TAbs x2 k2 t2) -> 
@@ -199,11 +200,11 @@ kindOf ctx (TApp t1 t2) = do
         KArrow k1' k2' -> if k1' == k2 then return k2' else Nothing
         _ -> Nothing
 kindOf ctx (TAbs v k1 typ) = do
-    let extended = extendTypeVar v k1 TTop ctx
+    let extended = extendTypeVarBound v TTop k1 ctx
     k2 <- kindOf extended typ 
     return (KArrow k1 k2)
-kindOf ctx (TForall x typ) = do
-    let extended = extendTypeVar x KStar ctx 
+kindOf ctx (TForall x b typ) = do
+    let extended = extendTypeVarBound x b KStar ctx 
     k <- kindOf extended typ
     if k == KStar then return KStar else Nothing
 
@@ -220,28 +221,33 @@ typeOf ctx (App t1 t2) = do
     t2' <- typeOf ctx t2
     case simplifyType t1' of 
         TArrow t11 t12 -> 
-            if typeEquiv t11 t2' then return t2'
+            if typeEquiv t11 t2' then return t12
                 else Nothing
-typeOf ctx (TyAbs v k t) = do
-    let extended = extendTypeVar v k ctx 
-    t' <- typeOf extended t 
-    return $ TForall v t'
+typeOf ctx (TyAbs x bound t) = do
+    boundKind <- kindOf ctx bound 
+    if boundKind /= KStar then Nothing else do 
+        let ctx' = extendTypeVarBound x bound KStar ctx
+        t' <- typeOf ctx' t 
+        return $ TForall x bound t'
 typeOf ctx (TyApp t typ) = 
     case kindOf ctx typ of
         Just k -> do 
             t' <- typeOf ctx t
             case simplifyType t' of 
-                TForall v body -> return $ substType v typ body
-                _ -> Nothin
-                g
-isSubtype :: Context -> Type -> Type -> Bool 
-isSubtype ctx t1 t2 = do
-    k1 <- kindOf ctx t1
-    k2 <- kindOf ctx t2 
-    if k1 /= k2 
-        then False 
-        else isSubtypeSameKind ctx t1 t2
+                TForall v bound body -> 
+                    if isSubtype ctx typ bound
+                        then return $ substType v typ body
+                        else Nothing
+                _ -> Nothing
 
+isSubtype :: Context -> Type -> Type -> Bool
+isSubtype ctx t1 t2 = 
+    case (kindOf ctx t1, kindOf ctx t2) of
+        (Just k1, Just k2) -> 
+            if k1 /= k2
+                then False
+                else isSubtypeSameKind ctx t1 t2
+        _ -> False
 isSubtypeSameKind :: Context -> Type -> Type -> Bool 
 isSubtypeSameKind ctx s TTop = let s' = kindOf ctx s 
     in s' == Just KStar
@@ -251,3 +257,11 @@ isSubtypeSameKind ctx (TVar x) t =
     case lookupTypeVarBound x ctx of 
         Just bound -> if bound == t then True else isSubtype ctx bound t 
         Nothing -> TVar x == t
+isSubtypeSameKind ctx (TForall x1 b1 s2) (TForall x2 b2 t2) =
+    if typeEquiv b1 b2 then
+    let fresh = freshVar x1 (Set.union (freeTypeVars s2) (freeTypeVars t2))
+        extended = extendTypeVarBound fresh b1 KStar ctx
+        s2' = substType x1 (TVar fresh) s2 
+        t2' = substType x2 (TVar fresh) t2
+    in isSubtype extended s2' t2'
+    else False
